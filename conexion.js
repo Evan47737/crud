a223ae31f283692b11ef523d58ee3bc08d417283
Evan47737 <eypm60322@gmail.com>
2025-07-12 11:17:53 -0600

    conexion.js

---
+++ conexion.js
@@ -0,0 +1,9 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+     apiKey: "AIzaSyDlm7IAFgI5ptaIcffQ15qCPxTWcBYaPQc",
+    authDomain: "crud-2701c.firebaseapp.com",
+    projectId: "crud-2701c",
+    storageBucket: "crud-2701c.firebasestorage.app",
+    messagingSenderId: "989102089265",
+    appId: "1:989102089265:web:5fc612de575313a5fb2110"
+});
